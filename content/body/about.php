c5c20217ba1b1089cca474919ab12544dd1f61b2
Unknown <Saulsanper98@gmail.com>
2016-11-10 18:06:02 +0000

    Added Bootstrap 4

--- content/body/about.php
+++ content/body/about.php
@@ -1,10 +1,4 @@
-<div class="container">
-    
-    <div class="jumbotron">
-        <div class="container">
-            <h1>Hola!!</h1>
-            <p>Mi nombre es Saúl Sánchez Pérez y soy estudiante del ciclo medio de Sistemas Microinformaticos y Redes en el CIFP Villa de Agüimes</p>
-        </div>
-    </div>
-    
+<div class="jumbotron">
+        <h1>¡Hola!</h1>
+        <p>Mi nombre es Saúl Sánchez Pérez y soy estudiante del ciclo medio de Sistemas Microinformaticos y Redes en el CIFP Villa de Agüimes</p>
 </div>
--- content/footer.php
+++ content/footer.php
@@ -1,11 +1,11 @@
-        
-        <footer class="footer"><center>
-            &copy; SaulSanchez
-        </footer></center>
+      <footer class="footer">
+        <p>&copy; Company 2014</p>
+      </footer>
 
+        </div> <!-- /container -->
         <!-- jQuery -->
         <script src="js/jquery-3.1.1.min.js"></script>
         <!-- Bootstrap JavaScript -->
         <script src="js/bootstrap.min.js"></script>
-    </body>
+  </body>
 </html>
