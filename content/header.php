d8a5906b43e58a36d43b38d2c0eca9cdd60eddd4
Unknown <Saulsanper98@gmail.com>
2016-11-11 12:30:40 +0000

    Included brand image

--- content/header.php
+++ content/header.php
@@ -13,6 +13,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="css/cover.css" rel="stylesheet">
@@ -28,7 +29,7 @@
 
           <div class="masthead clearfix">
             <div class="inner">
-              <h3 class="masthead-brand">Saúl Sánchez Pérez</h3>
+              <h3 class="masthead-brand"><img class="img-responsive imgright" width="35" height="35" src="../../img/S.png">Saúl Sánchez Pérez</h3>
               <nav class="nav nav-masthead">
                 <a class="nav-link <?php echo $content == 'home' ? 'active' : ''; ?>" href="?p=home">Home</a>
                 <a class="nav-link <?php echo $content == 'about' ? 'active' : ''; ?>" href="?p=about">About me</a>
